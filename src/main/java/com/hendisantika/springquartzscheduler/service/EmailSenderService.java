c9193acdff3b6950cb31fe7f9a33e6190b8eacac
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 05:33:11 +0700

    Add sendEmailWithAttachment in EmailSenderService🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/springquartzscheduler/service/EmailSenderService.java
+++ src/main/java/com/hendisantika/springquartzscheduler/service/EmailSenderService.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springquartzscheduler.service;
 
 import com.hendisantika.springquartzscheduler.model.Email;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,4 +37,20 @@ public class EmailSenderService {
         mailSender.send(message);
     }
 
+    public void sendEmailWithAttachment(Email email, String attachment) throws MessagingException {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom(email.getFrom());
+        mimeMessageHelper.setTo(email.getTo());
+        mimeMessageHelper.setSubject(email.getSubject());
+        mimeMessageHelper.setText(email.getText());
+
+        FileSystemResource fileSystem = new FileSystemResource(new File(attachment));
+
+        mimeMessageHelper.addAttachment(fileSystem.getFilename(), fileSystem);
+
+        mailSender.send(mimeMessage);
+    }
 }
