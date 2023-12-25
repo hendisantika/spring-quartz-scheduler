2a2c478fe6f4170e6def9b7f1af013ecaefd634b
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 05:34:44 +0700

    Add scheduleEmail in EmailSchedulerController🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/springquartzscheduler/controller/EmailSchedulerController.java
+++ src/main/java/com/hendisantika/springquartzscheduler/controller/EmailSchedulerController.java
@@ -1,10 +1,22 @@
 package com.hendisantika.springquartzscheduler.controller;
 
+import com.hendisantika.springquartzscheduler.model.EmailRequest;
+import com.hendisantika.springquartzscheduler.model.EmailResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +38,36 @@ public class EmailSchedulerController {
     @GetMapping("/ok")
     public String getHome() {
         return "All OK";
+    }
+
+    @PostMapping("/schedule/email")
+    public ResponseEntity<EmailResponse> scheduleEmail(@Valid @RequestBody EmailRequest emailRequest) {
+        try {
+            ZonedDateTime dateTime = ZonedDateTime.of(emailRequest.getDateTime(), emailRequest.getTimeZone());
+            if (dateTime.isBefore(ZonedDateTime.now())) {
+                EmailResponse emailResponse = new EmailResponse(false, "Datetime must be after current time");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(emailResponse);
+            }
+            JobDetail jobDetail = buildJobDetail(emailRequest);
+            Trigger trigger = buildTrigger(jobDetail, dateTime);
+
+            scheduler.scheduleJob(jobDetail, trigger);
+
+            EmailResponse emailResponse = new EmailResponse(
+                    true,
+                    jobDetail.getKey().getName(),
+                    jobDetail.getKey().getGroup(),
+                    "Email scheduled successfully"
+            );
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(emailResponse);
+        } catch (SchedulerException se) {
+            String msg = "Error while scheduling email";
+            log.error(msg + ": " + se);
+            EmailResponse emailResponse = new EmailResponse(false, msg);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(emailResponse);
+        }
     }
 }
