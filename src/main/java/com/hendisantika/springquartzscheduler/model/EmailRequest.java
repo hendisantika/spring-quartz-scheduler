7a73a986cf35bf4dbd51d2c9e3a0414ba51b67ae
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 05:30:26 +0700

    Add EmailRequest🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/springquartzscheduler/model/EmailRequest.java
@@ -0,0 +1,41 @@
+package com.hendisantika.springquartzscheduler.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-quartz-scheduler
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/26/23
+ * Time: 05:29
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class EmailRequest {
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String subject;
+
+    @NotEmpty
+    private String body;
+
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @NotNull
+    private ZoneId timeZone;
+}
